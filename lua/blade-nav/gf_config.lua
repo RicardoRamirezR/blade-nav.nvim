67889c39b2f9efe20f1b334e694bcd81966e0154
Ricardo Ramirez R <ricardo.ramirez.r@gmail.com>
2024-07-14 11:55:16 -0500

    feat(gf): navigate to a subkey. Closes #14.

--- lua/blade-nav/gf_config.lua
+++ lua/blade-nav/gf_config.lua
@@ -2,6 +2,26 @@ local utils = require("blade-nav.utils")
 
 local M = {}
 
+local function goto_subkey(ts, lang, node, keys)
+  if #keys == 0 then
+    return
+  end
+
+  local key_name = table.remove(keys, 1)
+  local query_template = [[ (string (string_content) @s (#eq? @s "%s")) ]]
+  local query_string = string.format(query_template, key_name)
+  local query = ts.query.parse(lang, query_string)
+
+  for _, matches, _ in query:iter_matches(node, 0) do
+    for _, node in pairs(matches) do
+      local start_row, start_col, _ = node:start()
+      vim.api.nvim_win_set_cursor(0, { start_row + 1, start_col })
+      goto_subkey(ts, lang, node, keys)
+      return
+    end
+  end
+end
+
 local function goto_keys(keys)
   local root, lang = utils.get_root_and_lang()
   if not root then
@@ -11,7 +31,8 @@ local function goto_keys(keys)
   local ts = vim.treesitter
   local query_template = [[
     (array_element_initializer
-      (string (string_content) @s (#eq? @s "%s"))
+      (string (string_content) @y (#eq? @y "%s"))
+      (array_creation_expression)
     )
   ]]
   local key_name = table.remove(keys, 1)
@@ -22,6 +43,7 @@ local function goto_keys(keys)
     for _, node in pairs(matches) do
       local start_row, start_col, _ = node:start()
       vim.api.nvim_win_set_cursor(0, { start_row + 1, start_col })
+      goto_subkey(ts, lang, node:parent():parent(), keys)
       return
     end
   end
